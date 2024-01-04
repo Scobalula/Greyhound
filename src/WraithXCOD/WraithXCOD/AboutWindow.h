65e67f046af3f5651a98b3b4a8554532d8d42713
dest1yo <hiangu@qq.com>
2024-01-05 01:10:57 +0800

    New discord server info

--- src/WraithXCOD/WraithXCOD/AboutWindow.h
+++ src/WraithXCOD/WraithXCOD/AboutWindow.h
@@ -30,9 +30,9 @@ protected:
         // Setup about
         this->AddColorText("Greyhound - A continuation of Wraith Archon, The Call of Duty asset exporter\n", RGB(0, 102, 255), 14, true);
         this->AddColorText("Developed by ", RGB(255, 255, 255), 13);
-        this->AddColorText("DTZxPorter", RGB(236, 52, 202), 13, true);
+        this->AddColorText("DTZxPorter & Scobalula", RGB(236, 52, 202), 13, true);
         this->AddColorText(" - Maintained by ", RGB(255, 255, 255), 13);
-        this->AddColorText("Scobalula", RGB(52, 152, 219), 13, true);
+        this->AddColorText("dest1yo", RGB(52, 152, 219), 13, true);
         this->AddColorText(" - Some game research by ", RGB(255, 255, 255), 13);
         this->AddColorText("id-daemon and help from Eric Maynard's ModelGetter", RGB(52, 152, 219), 13, true);
         this->AddColorText(".\n", RGB(255, 255, 255), 13);
